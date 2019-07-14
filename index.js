5094e09c113134271fbc790e4a52d6bbe2bfedc0
Gabriel Diaz <gabriel.d.diaz@oracle.com>
2019-07-13 21:22:45 -0300

    turn off robot

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const utils = require('./commands/utils.js');
 
+/*
 client.login(config.discordToken);
 bot.botActions(client);
 
@@ -27,4 +28,4 @@ app.get('/', function (req, res) {
 
 app.listen(PORT, function () {
     console.log('Example app listening on port 5000!');
-});
+});*/
